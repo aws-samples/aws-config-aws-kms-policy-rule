bdfb9ee9b341cb4a44211bbad5964633786773d6
Tracy <49311871+TPierce-2014@users.noreply.github.com>
2022-11-30 10:59:58 -0600

    Update AWSConfigRuleKMS.py
    Adding Helper Functions

--- AWSConfigRuleKMS.py
+++ AWSConfigRuleKMS.py
@@ -288,3 +288,242 @@ def evaluate_compliance(event, configuration_item, rule_parameters):
                 )
                 evaluations.append(ev)
     return evaluations
+
+
+# USE AS IS
+# Helper function to check if rule parameters exist
+def parameters_exist(parameters):
+    return len(parameters) != 0
+
+
+# Helper function used to validate input
+def check_defined(reference, referenceName):
+    if not reference:
+        raise Exception("Error: ", referenceName, "is not defined")
+    return reference
+
+
+# Check whether the message is OversizedConfigurationItemChangeNotification or not
+def is_oversized_changed_notification(messageType):
+    check_defined(messageType, "messageType")
+    return messageType == "OversizedConfigurationItemChangeNotification"
+
+
+# Check whether the message is a ScheduledNotification or not.
+def is_scheduled_notification(messageType):
+    check_defined(messageType, "messageType")
+    return messageType == "ScheduledNotification"
+
+
+# Get configurationItem using getResourceConfigHistory API
+# in case of OversizedConfigurationItemChangeNotification
+def get_configuration(resourceType, resourceId, configurationCaptureTime):
+    result = AWS_CONFIG_CLIENT.get_resource_config_history(
+        resourceType=resourceType,
+        resourceId=resourceId,
+        laterTime=configurationCaptureTime,
+        limit=1,
+    )
+    configurationItem = result["configurationItems"][0]
+    return convert_api_configuration(configurationItem)
+
+
+# Convert from the API model to the original invocation model
+def convert_api_configuration(configurationItem):
+    for k, v in configurationItem.items():
+        if isinstance(v, datetime.datetime):
+            configurationItem[k] = str(v)
+    configurationItem["awsAccountId"] = configurationItem["accountId"]
+    configurationItem["ARN"] = configurationItem["arn"]
+    configurationItem["configurationStateMd5Hash"] = configurationItem[
+        "configurationItemMD5Hash"
+    ]
+    configurationItem["configurationItemVersion"] = configurationItem["version"]
+    configurationItem["configuration"] = json.loads(configurationItem["configuration"])
+    if "relationships" in configurationItem:
+        for i in range(len(configurationItem["relationships"])):
+            configurationItem["relationships"][i]["name"] = configurationItem[
+                "relationships"
+            ][i]["relationshipName"]
+    return configurationItem
+
+
+# Based on the type of message get the configuration item
+# either from configurationItem in the invoking event
+# or using the getResourceConfigHistiry API in getConfiguration function.
+def get_configuration_item(invokingEvent):
+    check_defined(invokingEvent, "invokingEvent")
+    if is_oversized_changed_notification(invokingEvent["messageType"]):
+        configurationItemSummary = check_defined(
+            invokingEvent["configurationItemSummary"], "configurationItemSummary"
+        )
+        return get_configuration(
+            configurationItemSummary["resourceType"],
+            configurationItemSummary["resourceId"],
+            configurationItemSummary["configurationItemCaptureTime"],
+        )
+    elif is_scheduled_notification(invokingEvent["messageType"]):
+        return None
+    return check_defined(invokingEvent["configurationItem"], "configurationItem")
+
+
+# Check whether the resource has been deleted. If it has, then the evaluation is unnecessary.
+def is_applicable(configurationItem, event):
+    check_defined(configurationItem, "configurationItem")
+    check_defined(event, "event")
+    status = configurationItem["configurationItemStatus"]
+    eventLeftScope = event["eventLeftScope"]
+    if status == "ResourceDeleted":
+        print("Resource Deleted, setting Compliance Status to NOT_APPLICABLE.")
+    return (status == "OK" or status == "ResourceDiscovered") and not eventLeftScope
+
+
+# This gets the client after assuming the Config service role
+# either in the same AWS account or cross-account.
+def get_client(service, event=None):
+    if not event:
+        return boto3.client(service)
+    credentials = get_assume_role_credentials(event["executionRoleArn"])
+    return boto3.client(
+        service,
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
+    )
+
+
+def get_assume_role_credentials(role_arn):
+    sts_client = boto3.client("sts")
+    try:
+        assume_role_response = sts_client.assume_role(
+            RoleArn=role_arn, RoleSessionName="configLambdaExecution"
+        )
+        return assume_role_response["Credentials"]
+    except botocore.exceptions.ClientError as ex:
+        # Scrub error message for any internal account info leaks
+        if "AccessDenied" in ex.response["Error"]["Code"]:
+            ex.response["Error"][
+                "Message"
+            ] = "AWS Config does not have permission to assume the IAM role."
+        else:
+            ex.response["Error"]["Message"] = "InternalError"
+            ex.response["Error"]["Code"] = "InternalError"
+        raise ex
+
+
+# This generate an evaluation for config
+def build_evaluation(
+    resource_id,
+    compliance_type,
+    event,
+    resource_type=DEFAULT_RESOURCE_TYPE,
+    annotation=None,
+):
+    eval_cc = {}
+    if annotation:
+        eval_cc['Annotation'] = annotation
+    eval_cc['ComplianceResourceType'] = resource_type
+    eval_cc['ComplianceResourceId'] = resource_id
+    eval_cc['ComplianceType'] = compliance_type
+    eval_cc['OrderingTimestamp'] = str(
+        json.loads(event['invokingEvent'])['notificationCreationTime']
+    )
+    return eval_cc
+
+
+def build_evaluation_from_config_item(
+    configuration_item, compliance_type, annotation=None
+):
+    # print(configuration_item)
+    # print(compliance_type)
+    eval_ci = {}
+    if annotation:
+        eval_ci["Annotation"] = annotation
+    eval_ci["ComplianceResourceType"] = configuration_item['resourceType']
+    eval_ci["ComplianceResourceId"] = configuration_item['resourceId']
+    eval_ci["ComplianceType"] = compliance_type
+    eval_ci["OrderingTimestamp"] = configuration_item["configurationItemCaptureTime"]
+    return eval_ci
+
+
+# This decorates the lambda_handler in rule_code with the actual PutEvaluation call
+def lambda_handler(event, context):
+    # print(json.dumps(event))
+    global AWS_CONFIG_CLIENT
+    if ASSUME_ROLE_MODE:
+        AWS_CONFIG_CLIENT = get_client("config", event)
+
+    evaluations = []
+
+    # print(event)
+    check_defined(event, "event")
+    invokingEvent = json.loads(event["invokingEvent"])
+    rule_parameters = {}
+    if "ruleParameters" in event:
+        rule_parameters = json.loads(event["ruleParameters"])
+
+    configuration_item = get_configuration_item(invokingEvent)
+
+    compliance_result = evaluate_compliance(event, configuration_item, rule_parameters)
+    # print(compliance_result)
+    # print(type(compliance_result))
+
+    if isinstance(compliance_result, str):
+        if configuration_item:
+            evaluations.append(
+                build_evaluation_from_config_item(configuration_item, compliance_result)
+            )
+        else:
+            evaluations.append(
+                build_evaluation(
+                    event['accountId'],
+                    compliance_result,
+                    event,
+                    resource_type=DEFAULT_RESOURCE_TYPE,
+                )
+            )
+    elif isinstance(compliance_result, list):
+        for evaluation in compliance_result:
+            missing_fields = False
+            for field in (
+                "ComplianceResourceType",
+                "ComplianceResourceId",
+                "ComplianceType",
+                "OrderingTimestamp",
+            ):
+                if field not in evaluation:
+                    print("Missing " + field + " from custom evaluation.")
+                    missing_fields = True
+
+            if not missing_fields:
+                evaluations.append(evaluation)
+    elif isinstance(compliance_result, dict):
+        missing_fields = False
+        for field in (
+            "ComplianceResourceType",
+            "ComplianceResourceId",
+            "ComplianceType",
+            "OrderingTimestamp",
+        ):
+            if field not in compliance_result:
+                print("Missing " + field + " from custom evaluation.")
+                missing_fields = True
+        if not missing_fields:
+            evaluations.append(compliance_result)
+    else:
+        evaluations.append(
+            build_evaluation_from_config_item(configuration_item, "NOT_APPLICABLE")
+        )
+
+    # Put together the request that reports the evaluation status
+    resultToken = event["resultToken"]
+    testMode = False
+    if resultToken == "TESTMODE":
+        # Used solely for RDK test to skip actual put_evaluation API call
+        testMode = True
+    # Invoke the Config API to report the result of the evaluation
+    AWS_CONFIG_CLIENT.put_evaluations(
+        Evaluations=evaluations, ResultToken=resultToken, TestMode=testMode
+    )
+    # Used solely for RDK test to be able to test Lambda function
+    return evaluations
